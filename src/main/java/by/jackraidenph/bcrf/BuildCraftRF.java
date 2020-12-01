75177b8348975c77d27ab1240d0491a1f1258785
Jack Raiden <irinadz09@yandex.by>
2020-12-01 18:53:03 +0300

    Method name is now more obvious.

--- src/main/java/by/jackraidenph/bcrf/BuildCraftRF.java
+++ src/main/java/by/jackraidenph/bcrf/BuildCraftRF.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
@@ -45,7 +46,6 @@ public class BuildCraftRF {
             Field f = findField(te.getClass(), MjBattery.class);
             if (f != null) {
                 try {
-
                     f.setAccessible(true);
                     Class batteryClazz = f.get(te).getClass();
                     Field[] fieldsList = batteryClazz.getDeclaredFields();
@@ -77,7 +77,7 @@ public class BuildCraftRF {
     }
 
     @SubscribeEvent
-    public void test(ChunkEvent.Load e) {
+    public void onChunkLoad(ChunkEvent.Load e) {
         e.getChunk().getTileEntityMap().forEach((x, y) -> trySetValues(y));
     }
 
